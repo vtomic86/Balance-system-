a010fd416636401a2797eb8ab4d3eb1200c237a2
Vito Michele Di Lecce <vito141186@libero.it>
2016-08-24 16:22:21 +0200

    Add files via upload

---
+++ Interrupciones.ino
@@ -0,0 +1,31 @@
+
+/*
+ *  interrupciones para la cuadratura de encoders
+ *  
+ */
+void motorIzqInterA()
+{
+  
+  _IzqEncoderBSet = digitalReadFast(e_EncoderIzqPinB);   // lee en pin de entrada
+
+  //y ajusta el conteo 
+#ifdef encoderIzquierdoReversa
+  _ticksIzqEncoder -= _IzqEncoderBSet ? -1 : +1;
+#else
+  _ticksIzqEncoder += _IzqEncoderBSet ? -1 : +1;
+#endif
+}
+
+void motorDerInterA()
+{
+  
+  _DerEncoderBSet = digitalReadFast(e_EncoderDerPinB);  
+
+
+#ifdef RightEncoderIsReversed
+  _ticksDerEncoder -= _DerEncoderBSet ? -1 : +1;
+#else
+  _ticksDerEncoder += _DerEncoderBSet ? -1 : +1;
+#endif
+}
+
